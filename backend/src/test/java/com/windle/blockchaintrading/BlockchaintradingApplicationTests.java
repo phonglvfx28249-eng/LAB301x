77cef69d88f1842aa9b7062a0f7c99ea4ee575bf
windle <phonglvfx28249@funix.edu.vn>
2026-07-15 12:29:56 +0700

    Initialize project

---
+++ backend/src/test/java/com/windle/blockchaintrading/BlockchaintradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windle.blockchaintrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchaintradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
